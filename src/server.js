71560abdca1aee2257e2469a94a6cec2a257f9d7
cardozov <vinicius.gcardozo@gmail.com>
2022-03-04 10:29:59 -0300

    unidade 6 - atividade 1

--- src/server.js
+++ src/server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
 
 const cepRota = require('./rotas/cep.rota');
-const itemRota = require('./rotas/item.rota');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/awari');
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 cepRota(app);
-itemRota(app);
 
 app.listen(3000, () => console.log('Rodando em localhost:3000'));
